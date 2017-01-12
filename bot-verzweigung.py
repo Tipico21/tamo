1075f4bfb00b2105f9e88df489fe0e4f94cf6747
kadirdmrr <kadirdemir0917@gmail.com>
2017-01-12 11:08:27 +0100

    Add files via upload

---
+++ bot-verzweigung.py
@@ -0,0 +1,25 @@
+
+input("hey herr Tamer\n-->")
+s=input("wie gehts dir\n-->")
+if "gut" in s:
+    v = input("ich hasse menschen die glücklich sind\n-->")
+    input("geh mir nicht auf die nerven, ich habe wichtigere sachen zu erledigen\nn-->")
+    
+
+elif "schlecht" in s:
+    t = input("bin ich dein psyschologe oder was?\n-->")
+    input(" du bist frech zum boss\n-->")
+    
+s=input("du kannst mein sklave werden, wenn du magst")
+if "ne" in s:
+    v=input("du spielst mit dem feuer\n-->")
+    input("das wird konsequenzen für dich haben\-->")
+
+
+elif "ok" in s:
+    t=input("du hast eine gute entscheidung getroffen\n-->")
+    input("guter junge\n-->")
+
+
+s=input("jetzt störe mich nicht weiter, ich bin schwer beschäftigt\n-->")
+
