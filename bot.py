e4c19aeb2d99961da50854e40984c835309a4806
kadirdmrr <kadirdemir0917@gmail.com>
2017-01-12 11:05:59 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,12 @@
+input("wg\n-->")
+
+
+input("was machst du\n-->")
+
+input("wie heißt du\n-->")
+
+
+input("was hast du heute noch vor :")
+
+
+input("du bist minderjährig, ich zeige deine eltern an :")
