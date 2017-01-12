04f5b127c064cf3962a8573094d9c0f707c79daf
kadirdmrr <kadirdemir0917@gmail.com>
2017-01-12 11:31:09 +0100

    Add files via upload

---
+++ bot-verzweigung_lesezeichen.py
@@ -0,0 +1,34 @@
+lesezeichen = 0
+def function():
+    global lesezeichen
+    lesezeichen += 1
+    if lesezeichen == 1:
+        print ("hey herr Tamer\n-->")
+        return
+    if lesezeichen == 2:
+        print("wie gehts dir\n-->")
+        return
+    if lesezeichen == 3 or leseichen == 4:
+        if "gut" in s:
+            v = input("ich hasse menschen die glücklich sind\n-->")
+            input("geh mir nicht auf die nerven, ich habe wichtigere sachen zu erledigen\nn-->")
+            
+
+        elif "schlecht" in s:
+            t = input("bin ich dein psyschologe oder was?\n-->")
+            input(" du bist frech zum boss\n-->")
+        
+    s=input("du kannst mein sklave werden, wenn du magst")
+    if "ne" in s:
+        v=input("du spielst mit dem feuer\n-->")
+        input("das wird konsequenzen für dich haben\-->")
+
+
+    elif "ok" in s:
+        t=input("du hast eine gute entscheidung getroffen\n-->")
+        input("guter junge\n-->")
+
+
+s=input("jetzt störe mich nicht weiter, ich bin schwer beschäftigt\n-->")
+
+
